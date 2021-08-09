2343f3c2a442c52ea44a8c75f88a5ba27bf4879e
chornota <yuriichornota@gmail.com>
2021-08-09 20:00:05 +0300

    Update UserRepo.java
    tips

--- src/main/java/com/family/family/repo/UserRepo.java
+++ src/main/java/com/family/family/repo/UserRepo.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Repository
 public interface UserRepo extends CrudRepository<User, Long> {
     List<User> findAll();
-
+    //почитай про hql nativeQuery використовуються в специфічних випадках, коли не можна заюзати  hql
     @Query(value = "SELECT * FROM USER", nativeQuery = true)
     List<User> findAllUsers();
 
